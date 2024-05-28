8f0b9e725d922ac805fd5bbbc6f2d1608a86c2e1
jonatavsantos <surinegann@gmail.com>
2024-05-27 22:41:20 -0300

    Pratice Angular

---
+++ training/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Card1Component } from './card1/card1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Card1Component,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
